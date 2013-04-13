85a63a57b303ac68098b080bb07536248bd2a750
Uri <urke.leshem@gmail.com>
2013-04-13 19:26:53 +0300

    revert index.js maybe it'll fix the error

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,8 @@
-// var ig = rquire('../lib/instagram.js');
 
-module.exports = {
-  index: function(req, res){
-    res.render('index', { title: 'Seminargram' });
-  }
-  /*queryTag: function(query){
-    var tags = req.query.split(" ");
-    var tagInfo = [];
-    for(var i=0; i < tags.length; i++){
-      tagInfo[i] = ig.getTagInfo(tags[i]);
-    }
-  }*/
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+  res.render('index', { title: 'Seminargram' });
 };
