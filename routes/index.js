3792da1efca339aea9ff4e2af5c86b379ae66b49
Uri <urke.leshem@gmail.com>
2013-04-13 19:10:07 +0300

    fest instagram connection

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
+var ig = rquire('./lib/instagram.js');
 
-/*
- * GET home page.
- */
-
-exports.index = function(req, res){
-  res.render('index', { title: 'Seminargram' });
+module.exports = {
+  index: function(req, res){
+    res.render('index', { title: 'Seminargram' });
+  },
+  queryTag: function(query){
+    var tags = req.query.split(" ");
+    var tagInfo = [];
+    for(var i=0; i < tags.length; i++){
+      tagInfo[i] = ig.getTagInfo(tags[i]);
+    }
+  }
 };
