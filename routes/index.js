00c3014b30b2cd6c3723d805a717ca869c99cfa0
Uri <urke.leshem@gmail.com>
2013-04-13 19:16:58 +0300

    test instagram connection

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
-var ig = rquire('./lib/instagram.js');
+// var ig = rquire('../lib/instagram.js');
 
 module.exports = {
   index: function(req, res){
     res.render('index', { title: 'Seminargram' });
-  },
-  queryTag: function(query){
+  };
+  /*queryTag: function(query){
     var tags = req.query.split(" ");
     var tagInfo = [];
     for(var i=0; i < tags.length; i++){
       tagInfo[i] = ig.getTagInfo(tags[i]);
     }
-  }
+  }*/
 };
