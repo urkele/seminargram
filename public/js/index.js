5144d95b8a0455d528ff4dbfa56cacec7a1b86e1
Uri <urke.leshem@gmail.com>
2013-04-19 15:38:16 +0300

    Client: Start working with returned data. just something initial

--- public/js/index.js
+++ public/js/index.js
@@ -7,6 +7,12 @@ $(document).ready(function(){
     e.preventDefault();
     $.get(url,{tags: query},
       function(data){
+        for (var i = 0; i < data.length; i++) {
+          console.log(data[i]);
+          var img = document.createElement("img");
+          img.src = data[i].recentImage.imageUrl;
+          $("#result").append(img);
+        };
         console.log("succesfuly got data: ", data);
       },"json")
   });
