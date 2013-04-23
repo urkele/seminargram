3a4bf8eee043ac0b1acba507d17d26e21021ed6a
Uri <urke.leshem@gmail.com>
2013-04-23 12:15:29 +0300

    cleanup

--- app.js
+++ app.js
@@ -37,6 +37,5 @@ app.post('/subscriptions', gramroutes.gotSubscription)
 
 server.listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
-  console.log("new branch");
 });
 gramroutes.createSocket(server);
