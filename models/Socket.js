6f1f4980d418139013b594a4fa3c7c48d3d09f2a
Uri Leshem <urke.leshem@gmail.com>
2013-07-03 23:47:41 +0300

    subscribing adds the client to a room named according to the tag's name

--- models/Socket.js
+++ models/Socket.js
@@ -2,10 +2,13 @@ var Backbone = require('backbone'),
     BackboneRelational = require('backbone-relational');
 
 var Socket = Backbone.RelationalModel.extend({
+    defaults: {
+        io: {}
+    },
 
     initialize: function (server) {
-        console.log('starting socket');
-        io = require('socket.io').listen(server);
+        this.set('io', require('socket.io').listen(server));
+        var io = this.get('io');
         io.configure(function () {
             io.set('flash policy server', false);
             io.set('log level', 1); // set socket.io logging level to 'warn'
@@ -19,59 +22,3 @@ var Socket = Backbone.RelationalModel.extend({
 });
 
 module.exports.Socket = Socket;
-/*
-    createSocket: function (server) {
-        io = require('socket.io').listen(server);
-        io.configure(function () {
-            io.set('flash policy server', false);
-            io.set('log level', 1); // set socket.io logging level to 'warn'
-        });
-        io.sockets.on('connection', function (socket) {
-            socket.emit('connection', 'connected');
-            socket.on('init', function (requestedTagsDirty) {
-                console.log("@gramroutes.createSocket - got init form client:", requestedTagsDirty);
-                if (requestedTagsDirty && requestedTagsDirty.length !== 0) {
-                    //TODO: also clean duplicate tags and clean non alpha numeric letters
-                    for (var badIndex = requestedTagsDirty.indexOf(""); badIndex > -1; badIndex = requestedTagsDirty.indexOf("")) {
-                        requestedTagsDirty.splice(badIndex, 1);
-                    }
-                    var requestedTags = requestedTagsDirty;
-                };
-                if (requestedTags && requestedTags.length !== 0) {
-                    getInitialData(requestedTags, socket.id, function(err, data) {
-                        if (err) {
-                            console.log("@gramroutes.createSocket - error getting initial data:", err);
-                        }
-                        else {
-                            // console.log("@gramroutes.createSocket - got initial data:", data);
-                            socket.emit('newData', data);
-                        };
-                    })
-                }
-                else {
-                    console.log("@gramroutes.createSocket - no tags requested")
-                }
-            });
-            socket.on('subscriptions', function (data) {
-                console.log("@gramroutes.createSocket - got 'subscriptions' form client", data);
-                if (data.handle) {
-                    if (data.handle == "stop") {
-                        unsubscribeAll(function (data) {
-                            socket.emit('debug', {"unsubscribed": data});
-                        });
-                    }
-                    else if (data.handle == "unsubscribe") {
-                        unregisterTags(data.tags, socket.id, function (err) {
-                            if (err) {
-                                console.log("@gramroutes.createSocket - error unsubscribing tags:", err);
-                            }
-                            else {
-                                socket.emit('debug', {"unsubscribedTags": data.tags});
-                            }
-                        });
-                    }
-                }
-            });
-        });
-    }
-*/
