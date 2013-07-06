2eef441021bf0eed75fd2754bad6421370e52cf8
Uri Leshem <urke.leshem@gmail.com>
2013-07-06 18:12:20 +0300

    implement socket-io mongoDB store

--- models/Socket.js
+++ models/Socket.js
@@ -1,17 +1,27 @@
 var Backbone = require('backbone'),
-    BackboneRelational = require('backbone-relational');
+    BackboneRelational = require('backbone-relational'),
+    MongoStore = require('mong.socket.io');
 
 var Socket = Backbone.RelationalModel.extend({
     defaults: {
-        io: {}
+        io: {},
+        mongoUrl: {
+            production: 'mongodb://sultagit:hazulit@'+process.env.MONGO_URI,
+            development: 'mongodb://sultagit-dev:hazulit@'+process.env.MONGO_URI,
+            local: 'mongodb://localhost:27017/sultagit-local'
+        }
     },
 
     initialize: function (server) {
         this.set('io', require('socket.io').listen(server));
         var io = this.get('io');
+        var store = new MongoStore({url: this.get('mongoUrl')[process.env.NODE_ENV]});
+        console.log('env is %s and its mongoUrl is ', process.env.NODE_ENV, this.get('mongoUrl')[process.env.NODE_ENV]);
         io.configure(function () {
             io.set('flash policy server', false);
-            io.set('log level', 1); // set socket.io logging level to 'warn'
+            io.set('log level', (process.env.NODE_ENV == 'production') ? 1 : 2); // set socket.io logging level to 'warn'
+            store.on('error', console.error);
+            io.set('store', store);
         });
     },
 
